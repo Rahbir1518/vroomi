11cb2b54d163cf2a55ad5e1e0183c08165365308
Rahbir1518 <rahbir1518@gmail.com>
2025-06-21 00:45:58 -0400

    INIT

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+
+  return (
+    <>
+      <div className='text-amber-700'>
+        Hello world
+      </div>
+    </>
+  )
+}
+
+export default App
