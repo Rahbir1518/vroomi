e9a693ac1e85c4a39676eb9a59207815040333e1
Hamzap01 <hamzapanwala2004@gmail.com>
2025-06-21 10:15:41 -0400

    vroomi logo

--- src/Driver.tsx
+++ src/Driver.tsx
@@ -1,6 +1,7 @@
 import { useUser, UserButton } from "@clerk/clerk-react";
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import logo from "../image/logo1.png"
 
 export default function Driver() {
   const { user } = useUser();
@@ -13,8 +14,13 @@ export default function Driver() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center py-4">
             <div className="flex items-center space-x-4">
-              <Link to="/" className="text-2xl font-bold text-blue-600">VROOMI</Link>
-              <span className="px-3 py-1 bg-blue-100 text-blue-700 text-sm font-medium rounded-full">
+              <Link to="/" className="">
+              <img
+                src={logo} alt="Vroomi" 
+                className="w-35 md:w-37 h-15 object-cover"
+                />
+            </Link>
+              <span className="px-3 py-1 bg-green-100 text-green-700 text-sm font-medium rounded-full">
                 Driver Dashboard
               </span>
             </div>
