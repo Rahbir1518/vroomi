b755512316c30da5707711329bf5eee6af233eb4
Rahbir1518 <rahbir1518@gmail.com>
2025-06-21 02:15:57 -0400

    color added in index

--- src/Welcome.tsx
+++ src/Welcome.tsx
@@ -4,9 +4,22 @@ export default function Welcome() {
   const navigate = useNavigate()
 
   return (
-    <div>
-      <h1>Welcome</h1>
-      <button onClick={() => navigate('/choose')}>Go to Choose</button>
+    
+    <div className="grid grid-cols-4 grid-rows-5 gap-4 h-screen bg-primary">
+      
+      
+        <div className="col-span-4 row-span-2 flex items-center justify-center">
+          <h1 className='font-primary text-5xl md:text-6xl font-bold text-tertiary'>Welcome</h1>
+        </div>
+
+        <div className="col-span-2 row-span-2 row-start-3 bg-amber-600">6</div>
+        
+        <div className="col-span-2 row-span-2 col-start-3 row-start-3 bg-green-500">7</div>
+        
+        <div className="col-span-4 row-start-5 bg-blue-500">
+          <button onClick={() => navigate('/choose')}>Go to Choose</button>
+        </div>
+
     </div>
   )
 }
